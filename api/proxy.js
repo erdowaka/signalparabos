7c73cd95c5c1dd1a2361cf8ece4bb8eaae904185
Erdo Waka Septyano <32562893+erdowaka@users.noreply.github.com>
2026-06-17 19:39:47 +0700

    test
    hh

--- api/proxy.js
+++ api/proxy.js
@@ -3,9 +3,11 @@ export default async function handler(req, res) {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: 'URL diperlukan' });
   try {
-    const response = await fetch(decodeURIComponent(url));
+    const response = await fetch(decodeURIComponent(url), {
+      headers: { 'User-Agent': 'Mozilla/5.0' }
+    });
     const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Bypass agar frontend bisa baca
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (err) {
     res.status(500).json({ error: 'Proxy gagal' });
